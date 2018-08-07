1d54aafe05b6c196591f6de91942ed24fe01a7c0
s17004 <s17004@std.it-college.ac.jp>
2018-08-07 15:04:22 +0900

    演習6-7

---
+++ 0802/En6_7.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+class En6_7 {
+  //--- 単純挿入ソート ---//
+  static void insertionSort(int[] a, int n) {
+    for (int i = 1; i < n; i++) {
+      for (int m = 0; m < n; m++)
+        System.out.printf("%3d ", a[m]);
+      System.out.println();
+
+      int j;
+      int tmp = a[i];
+      for (j = i; j > 0 && a[j - 1] > tmp; j--)
+        a[j] = a[j - 1];
+      a[j] = tmp;
+
+      for (int m = 0; m < n; m++)
+       System.out.print((m==j) ? "  ^ ":(m>j&&i>m)?"----":(m==i)?"  + ":"    ");
+      System.out.println();
+
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner stdIn = new Scanner(System.in);
+
+    System.out.println("単純挿入ソート");
+    System.out.print("要素数：");
+    int nx = stdIn.nextInt();
+    int[] x = new int[nx];
+
+    for (int i = 0; i < nx; i++) {
+      System.out.print("x[" + i + "]：");
+      x[i] = stdIn.nextInt();
+    }
+
+    insertionSort(x, nx);            // 配列xを単純挿入ソート
+
+    System.out.println("昇順にソートしました。");
+    for (int i = 0; i < nx; i++)
+      System.out.println("x[" + i + "]＝" + x[i]);
+  }
+}
